5de6604176569de6d8e9d46f0fe54f392fc50ad7
michaelwro <michael.wrona77@gmail.com>
2024-02-25 05:20:58 +0000

    Add more quaternion functions, WIP

--- include/math/Quaternion.hpp
+++ include/math/Quaternion.hpp
@@ -12,25 +12,123 @@ LIBRARY DEPENDENCIES: (
 #include <cstddef>
 
 namespace sim {
-namespace math {
 
 class Quaternion {
 public:
-  double q[4] {1, 0, 0, 0};  //!< (--) Quaternion values
+  double values[4] {1.0, 0.0, 0.0, 0.0};  //!< (--) Quaternion values
 
 public:
   Quaternion() = default;
   ~Quaternion() = default;
 
-  Quaternion(const double qs, const double qx, const double qy,
+  Quaternion(const double qw, const double qx, const double qy,
              const double qz) noexcept;
 
-  constexpr std::size_t size() const noexcept { return 4; }
+  Quaternion(const Quaternion&) = default;
+  Quaternion& operator=(const Quaternion&) = default;
 
+  /**
+   * @brief Move construct a Quaternion.
+   *
+   * @param other Other quaternion.
+   */
+  Quaternion(Quaternion&& other);
+
+  /**
+   * @brief Move assign a Quaternion.
+   *
+   * @param other Other quaternion.
+   * @return This quaternion.
+   */
+  Quaternion& operator=(Quaternion&& other);
+
+  /**
+   * @brief Get the number of elements in the quaternion (four).
+   *
+   * @return Four.
+   */
+  static constexpr std::size_t size() noexcept { return 4; }
+
+  /**
+   * @brief Get a quaternion's value.
+   *
+   * @param index Quaternion index.
+   * @return Quaternion value.
+   */
+  const double& operator()(const std::size_t index) const;
+
+  /**
+   * @brief Get the quaternion scalar component.
+   *
+   * @return Scalar component.
+   */
+  const double& w() const { return values[0]; };
+
+  /**
+   * @brief Get the quaternion X-component.
+   *
+   * @return X-component.
+   */
+  const double& x() const { return values[1]; };
+
+  /**
+   * @brief Get the quaternion Y-component.
+   *
+   * @return Y-component.
+   */
+  const double& y() const { return values[2]; };
+
+  /**
+   * @brief Get the quaternion Z-component.
+   *
+   * @return Z-component.
+   */
+  const double& z() const { return values[3]; };
+
+  /**
+   * @brief Assign elements to a quaternion. Normalizes input.
+   *
+   * @param qw Scalar component.
+   * @param qx X-component.
+   * @param qy Y-component.
+   * @param qz Z-component.
+   * @return The quaternion.
+   */
+  static Quaternion assign(const double qw, const double qx, const double qy,
+                           const double qz);
+
+  /**
+   * @brief Normalize the quaternion.
+   */
   void normalize();
+
+  /**
+   * @brief Invert/conjugate this quaternion.
+   */
+  void invert();
+
+  /**
+   * @brief Get the inverse/conjugate of this quaternion.
+   *
+   * @return Quaternion inverse/conjugate.
+   */
+  Quaternion getInverse() const;
 };
 
-}  // namespace math
+/**
+ * @brief Get Quaternion element with compile-time bounds checks.
+ *
+ * @tparam index Quaternion index.
+ * @param quat The Quaternion.
+ * @return Quaternion value.
+ */
+template <std::size_t index>
+const double& get(const Quaternion& quat)
+{
+  static_assert(index < Quaternion::size(), "Invalid index.");
+  return quat(index);
+}
+
 }  // namespace sim
 
 #endif  // SIM_MATH_QUATERNION_HPP
--- src/math/Quaternion.cpp
+++ src/math/Quaternion.cpp
@@ -10,16 +10,62 @@ LIBRARY DEPENDENCIES: (
 
 #include <cassert>
 #include <cmath>
+#include <initializer_list>
 #include <limits>
 
 namespace sim {
-namespace math {
 
-Quaternion::Quaternion(const double qs, const double qx, const double qy,
+Quaternion::Quaternion(const double qw, const double qx, const double qy,
                        const double qz) noexcept
-    : q {qs, qx, qy, qz}
+    : values {qw, qx, qy, qz}
 {
-  // NORMALIZE
+  normalize();
+}
+
+/**
+ * @details Other quaternion becomes a unit quaternion.
+ */
+Quaternion::Quaternion(Quaternion&& other)
+{
+  values[0] = other.values[0];
+  other.values[0] = 1.0;
+
+  for (std::size_t index : {1, 2, 3}) {
+    values[index] = other.values[index];
+    other.values[index] = 0.0;
+  }
+}
+
+/**
+ * @details Other quaternion becomes a unit quaternion.
+ */
+Quaternion& Quaternion::operator=(Quaternion&& other)
+{
+  values[0] = other.values[0];
+  other.values[0] = 1.0;
+
+  for (std::size_t index : {1, 2, 3}) {
+    values[index] = other.values[index];
+    other.values[index] = 0.0;
+  }
+
+  return *this;
+}
+
+Quaternion Quaternion::assign(const double qw, const double qx, const double qy,
+                              const double qz)
+{
+  // NOTE: ctor normalizes
+  return Quaternion(qw, qx, qy, qz);
+}
+
+/**
+ * @details Includes runtime assert to check index.
+ */
+const double& Quaternion::operator()(const std::size_t index) const
+{
+  assert(index < Quaternion::size());
+  return values[index];
 }
 
 void Quaternion::normalize()
@@ -28,19 +74,32 @@ void Quaternion::normalize()
   double magnitude = 0.0;
 
   for (std::size_t idx = 0; idx < size(); idx++) {
-    magnitude += q[idx] * q[idx];
+    magnitude += values[idx] * values[idx];
   }
 
   // verify no-zero magnitude (div-by-zero)
-  assert(magnitude >= 8.0 * std::numeric_limits<double>::epsilon());
+  assert(magnitude >= std::numeric_limits<double>::epsilon());
 
   // normalize elements
   const double inv_norm = 1.0 / std::sqrt(magnitude);
 
   for (std::size_t idx = 0; idx < size(); idx++) {
-    q[idx] /= inv_norm;
+    values[idx] /= inv_norm;
   }
 }
 
-}  // namespace math
+void Quaternion::invert()
+{
+  values[1] *= -1.0;
+  values[2] *= -1.0;
+  values[3] *= -1.0;
+}
+
+Quaternion Quaternion::getInverse() const
+{
+  Quaternion quat(*this);
+  quat.invert();
+  return quat;
+}
+
 }  // namespace sim

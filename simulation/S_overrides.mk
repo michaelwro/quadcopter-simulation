2e53bfa0415967e5bdab682f3b23f3f2f72365f1
michaelwro <michael.wrona77@gmail.com>
2024-02-25 04:49:06 +0000

    Add path to include statements

--- simulation/S_overrides.mk
+++ simulation/S_overrides.mk
@@ -1,12 +1,15 @@
 # Define Trick build variable overrides
 
+# get the current path
+THIS_DIR=$(abspath $(dir $(lastword $(MAKEFILE_LIST))))
+
 # multithread build
 MAKEFLAGS += -j
 
 # # include paths
-TRICK_CFLAGS += -I../include/ -I../src/
-TRICK_CXXFLAGS += -I../include/ -I../src/
-TRICK_SFLAGS += -I../modules/
+TRICK_CFLAGS += -I${THIS_DIR}/../include/ -I${THIS_DIR}/../src/
+TRICK_CXXFLAGS += -I${THIS_DIR}/../include/ -I${THIS_DIR}/../src/
+TRICK_SFLAGS += -I${THIS_DIR}/../modules/
 
 # additional options
 TRICK_CFLAGS += -std=c11 -Wall -Wextra -Werror -Wpedantic

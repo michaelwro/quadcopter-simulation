782ef97e75eab162d9340b9165b69348d9e644d9
Michael Wrona <46735409+michaelwro@users.noreply.github.com>
2024-08-02 21:25:16 -0700

    Initial commit

---
+++ .cmake-format.py
@@ -0,0 +1,11 @@
+"""
+
+https://cmake-format.readthedocs.io/en/latest/configuration.html
+https://github.com/cheshirekow/cmake_format#configuration
+
+"""
+
+with section("format"):
+    line_width = 80
+    tab_size = 2
+    dangle_parens = True
---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <spdlog/spdlog.h>
+
+#include <Eigen/Dense>
+
+int main()
+{
+  // NEED TO INSTALL FMT FOR LOGGER
+  spdlog::set_level(spdlog::level::debug); // Set global log level to debug
+  spdlog::info("testing 123!!!");
+
+  Eigen::Matrix2d m;
+  m(0, 0) = 3;
+  m(1, 0) = 2.5;
+  m(0, 1) = -1;
+  m(1, 1) = m(1, 0) + m(0, 1);
+
+  return 0;
+}
